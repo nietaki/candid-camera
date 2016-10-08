ac5d7392f73196449ee855ac015629de6844cf59
Jacek Królikowski <nietaki@gmail.com>
2016-10-08 16:50:25 +0100

    added threshold to runner

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import image
 
 if __name__ == "__main__":
     print(sys.argv)
+    threshold = float(sys.argv[1])
     camera.take_picture("foo.jpg")
     camera.take_picture("bar.jpg")
-    print(image.similarity_f("pictures/foo.jpg", "pictures/bar.jpg"))
+    print(image.similarity_f("pictures/foo.jpg", "pictures/bar.jpg", threshold))
