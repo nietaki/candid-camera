3f1fce6d8070b124e19dbf11113f0ad9e7e651ae
Jacek Królikowski <nietaki@gmail.com>
2016-10-08 16:59:58 +0100

    debug messages

--- image.py
+++ image.py
@@ -1,10 +1,12 @@
-from scipy import ndimage
 from scipy import misc
 import numpy as np
 
 
 def read_as_1d_array(filename):
-    return flatten(read_as_array(filename))
+    print("reading as 1d array")
+    ret = flatten(read_as_array(filename))
+    print("read")
+    return ret
 
 def read_as_array(filename):
     return misc.imread(filename, flatten=True)
@@ -13,12 +15,14 @@ def flatten(ndarr):
     return np.reshape(ndarr, (1, -1))[0]
 
 def difference(flat1, flat2):
+    print("calculating diff")
     return abs(flat1 - flat2)
 
 def similarity(flat1, flat2, threshold = 25):
     diff = difference(flat1, flat2)
     # diff_count = len(list(filter(lambda x: x >= threshold, diff)))
-    diff_count = sum(d >= threshold for d in diff) 
+    "counting diff values"
+    diff_count = sum(d >= threshold for d in diff)
     return 1.0 - (diff_count / len(diff))
 
 def similarity_f(filename1, filename2, threshold = 25):
