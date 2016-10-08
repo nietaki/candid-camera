5b731c822993cd530848bf7b95e7b8cfd2cc4f1b
Jacek Królikowski <nietaki@gmail.com>
2016-10-08 16:49:18 +0100

    image similarity

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
+import camera
+import sys
+import image
+
 if __name__ == "__main__":
-    import camera
-    import sys
     print(sys.argv)
     camera.take_picture("foo.jpg")
+    camera.take_picture("bar.jpg")
+    print(image.similarity_f("pictures/foo.jpg", "pictures/bar.jpg"))
