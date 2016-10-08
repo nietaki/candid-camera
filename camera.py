8c0ca463b0cd93bba98e1f6f4b61a8614607b72d
Jacek Królikowski <nietaki@gmail.com>
2016-10-08 15:48:27 +0100

    camera module added

--- camera.py
+++ camera.py
@@ -1,3 +1,4 @@
-def take_picture(filename, delay=1, resolution="1280x720")
+def take_picture(filename, delay=1, resolution="1280x720"):
+    import os
     command_string = "fswebcam -r {0} --jpeg 95 -D {1} {2}".format(resolution, delay, filename)
     os.system(command_string)
---
+++ run.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    import camera
+    import sys
+    print(sys.argv)
+    camera.take_picture("pictures/foo.jpg")
